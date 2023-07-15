db2336d371ca02d41af96f4acf2d0ead6879129b
MarijusCoding <marijusdilys9@gmail.com>
2023-07-15 22:16:07 +0300

    Error handling

--- app/page.js
+++ app/page.js
@@ -1,7 +1,28 @@
-
+"use client"
+import axios from "axios"
+import { useEffect } from "react";
+import { useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState('');
+  useEffect(() => {
+    axios('https://jsonplaceholder.typicode.com/posts')
+    .then((data) => {
+      
+      setData(data)
+      console.log(data);
+    })
+    .catch((err) => {
+      throw err;
+    })
+  }, [])
   return (
-    
+    <>
+    {Object.keys(data).map((data) => {
+      return data.map(data) => {
+        {data.id}
+      }
+    })}
+    </>
   )
 }
